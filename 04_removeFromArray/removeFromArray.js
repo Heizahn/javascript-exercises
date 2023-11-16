43b6aa6e4a8a5a244273a6200b8e26752b61ce5a
Heizahn <humbertorbw@gmail.com>
2023-11-15 20:57:15 -0400

    exercise 04 complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, argument) {
+    let newArray = [];
 
+    for (let item of array) {
+        if (argument !== item) {
+            newArray.push(item);
+        };
+    };
+
+    return newArray;
 };
 
 // Do not edit below this line
