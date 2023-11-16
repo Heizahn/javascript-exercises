7f5ae3f1ef745eb480c9d1c3685f2754fe2862c8
Heizahn <humbertorbw@gmail.com>
2023-11-15 21:02:13 -0400

    exercise 05 complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sumTotal = 0;
 
+    for (let i = Math.min(num1, num2); i <= Math.max(num1, num2); i++) {
+        sumTotal += i;
+    };
+
+    return sumTotal;
 };
 
 // Do not edit below this line
