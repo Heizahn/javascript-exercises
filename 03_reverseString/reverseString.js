7175b4d8e21e56394615e8a80822cc802ff187c1
Heizahn <humbertorbw@gmail.com>
2023-11-15 20:53:40 -0400

    exercise 03 complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let result = '';
+    for (let i of str) {
+        result = i + result;
+    };
 
+    return result;
 };
 
 // Do not edit below this line
