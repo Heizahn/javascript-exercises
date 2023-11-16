c46cb5db14c3827653b3acc75132ac1d5a790332
Heizahn <humbertorbw@gmail.com>
2023-11-15 21:18:56 -0400

    exercise 07 complete

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(grade) {
+  let num = (grade - 32) * 5 / 9;
+  return Number(num.toFixed(2));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(grade) {
+  let num = grade * 9 / 5 + 32;
+  return Number(num.toFixed(2));
 };
 
 // Do not edit below this line
