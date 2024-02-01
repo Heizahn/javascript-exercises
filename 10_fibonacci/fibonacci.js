e3becd4ef6bc30d38c593a740a4809a0f1ca04a0
heizhan <humbertorbw@gmail.com>
2024-02-01 11:14:47 -0400

    fibonacci complete

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function (sequence) {
+	if (sequence > 0) {
+		let a = 0
+		let b = 1
+		let c = 0
 
+		for (let i = 0; i < sequence; i++) {
+			c = a + b
+			a = b
+			b = c
+		}
+
+		return a
+	}
+
+	return "OOPS"
 };
 
 // Do not edit below this line
