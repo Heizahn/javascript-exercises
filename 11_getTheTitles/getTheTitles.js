78d1e90c8879788b6af8767cc375fc1398399fff
heizhan <humbertorbw@gmail.com>
2024-02-01 11:20:00 -0400

    getTheTitles complete

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
+const getTheTitles = function (arrObj) {
+	let arrayTitle = []
 
+	for (let obj of arrObj) {
+		arrayTitle.push(obj.title)
+	}
+
+	return arrayTitle
 };
 
 // Do not edit below this line
