e9207c0591b06f78f3d5570bed2012842ba8249e
heizhan <humbertorbw@gmail.com>
2024-02-01 12:11:50 -0400

    findTheOldest complete

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function (arrObj) {
+	let actuality = (new Date).getFullYear()
+	let oldest = 0
+	let people = {}
 
+	for (let obj of arrObj) {
+		if (!("yearOfDeath" in obj)) {
+			obj.yearOfDeath = actuality
+		}
+
+		let age = obj.yearOfDeath - obj.yearOfBirth
+
+		if (oldest < age) {
+			oldest = age
+			people = obj
+		}
+	}
+
+	return people
 };
 
 // Do not edit below this line
 module.exports = findTheOldest;
+
